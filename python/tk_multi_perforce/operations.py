edc77021d24205ce54fd60d3d62049918af8cf06
infintropy <36432437+infintropy@users.noreply.github.com>
2021-10-08 11:43:46 -0400

    combine prep logic for asset, task, project, etc

--- python/tk_multi_perforce/operations.py
+++ python/tk_multi_perforce/operations.py
@@ -48,16 +48,48 @@ def revert_scene_changes(app):
 
 def open_sync_files_dialog(app, entity_type=None,  entity_ids=None):
     """
-    Prepare asset root paths to search in p4 and show the Perforce sync dialog
+    Prepare assets to send to the P4 Sync window to process
     """
     entities_to_sync = []
+
     if entity_type:
+        # if a single task were selected, or launched from a task detail page
         if entity_type == "Task":
             tasks = app.shotgun.find(entity_type, [['id', 'in', entity_ids]], ['entity'])
             entities_to_sync = [i.get('entity') for i in tasks if i.get('entity')]
+            entity_type = entities_to_sync[0].get('type')
+
+        # if assets were selected, make sure we have all the top level assets from child selections
+        if entity_type == "Asset":
+            ids = []
+            assets = app.shotgun.find(entity_type, [['id', 'in', entity_ids]], ['sg_asset_parent'])
+            parent_asset_ids = ids.extend([i.get('sg_asset_parent').get('id') for i in assets if i.get('sg_asset_parent')])
+            asset_ids = ids.extend([i.get('id') for i in assets if not i.get('sg_asset_parent')])    
+            entities_to_sync = [{"type": entity_type, "id": id} for id in list(set(ids))]
+            app.log_info(entities_to_sync)
+
+        # for other entity types, return the list of entity objects unmodified
         else:
             entities_to_sync = [{"type": entity_type, "id": id} for id in entity_ids]
         
+    else: # if user launching without context
+        # we look for all project tasks assigned to the current user
+        user = app.context.user
+        user_tasks = app.context.sgtk.shotgun.find("Task", 
+                                                   [["task_assignees", "is", user],["project", "is", app.context.project]], 
+                                                   ["entity"])
+    
+        # look through all the possible entity links to these tasks, and keep all the unique ones to send to the UI
+        user_assets = []
+        uids = []
+        for task in user_tasks:
+            linked_entity = task.get('entity')
+            if linked_entity:
+                uid = "{}_{}".format(linked_entity.get('type'), linked_entity.get('id'))
+                if uid not in uids:
+                    uids.append(uid)
+                    entities_to_sync.append(linked_entity)
+    
     try:
         p4_fw = sgtk.platform.get_framework("tk-framework-perforce")
         p4_fw.sync.sync_with_dialog(app, entities_to_sync)
